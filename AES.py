f9fcfc212c78f48dffe8147510a8093bd56088ee
Mohamed Khafagy <mohamed.tarek.khafagy@gmail.com>
2024-12-28 13:41:42 +0200

    Integrated the AES class with diffie hellman to achieve secure chatting

--- AES.py
+++ AES.py
@@ -1,77 +1,55 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
+import base64
 import os
 
-def generate_key_iv():
-    key = os.urandom(32)  # AES-256 key
-    iv = os.urandom(16)   # AES block size is 16 bytes
-    return key, iv
-    # iv make sure that identical plaintexts encrypt to different ciphertexts
-class BlockCipherHasher:
-    def __init__(self, key, iv):
-        self.key = key
-        self.iv = iv
+class AES:
 
-    def encrypt(self, data):
-        # Pad the data to be a multiple of the block size
-        padder = padding.PKCS7(algorithms.AES.block_size).padder()
-        padded_data = padder.update(data) + padder.finalize()
+    def encrypt_message(self, message, shared_key):
+        iv = os.urandom(12)
+        encryptor = Cipher(
+            algorithms.AES(shared_key),
+            modes.GCM(iv),
+            backend=default_backend()
+        ).encryptor()
 
-        # Create a Cipher object
-        cipher = Cipher(algorithms.AES(self.key), modes.CBC(self.iv), backend=default_backend())
-        encryptor = cipher.encryptor()
+        ciphertext = encryptor.update(message.encode()) + encryptor.finalize()
+        return base64.b64encode(iv + encryptor.tag + ciphertext).decode('utf-8')
 
-        # Encrypt the padded data
-        encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
-        return encrypted_data
-    
-    def decrypt(self, encrypted_data):
-        # Create a Cipher object
-        cipher = Cipher(algorithms.AES(self.key), modes.CBC(self.iv), backend=default_backend())
-        decryptor = cipher.decryptor()
+    def decrypt_message(self, encrypted_message, shared_key):
+        encrypted_data = base64.b64decode(encrypted_message.encode('utf-8'))
+        iv = encrypted_data[:12]
+        tag = encrypted_data[12:28]
+        ciphertext = encrypted_data[28:]
 
-        # Decrypt the data
-        padded_data = decryptor.update(encrypted_data) + decryptor.finalize()
+        decryptor = Cipher(
+            algorithms.AES(shared_key),
+            modes.GCM(iv, tag),
+            backend=default_backend()
+        ).decryptor()
 
-        # Unpad the data
-        unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
-        data = unpadder.update(padded_data) + unpadder.finalize()
-        return data
+        return decryptor.update(ciphertext).decode() + decryptor.finalize().decode()
 
-    def encrypt_text(self, text):
-        return self.encrypt(text.encode())
-    
-    def decrypt_text(self, encrypted_text):
-        return self.decrypt(encrypted_text).decode()
+    def encrypt_file(self, file_data, shared_key):
+        iv = os.urandom(12)
+        encryptor = Cipher(
+            algorithms.AES(shared_key),
+            modes.GCM(iv),
+            backend=default_backend()
+        ).encryptor()
 
+        ciphertext = encryptor.update(file_data) + encryptor.finalize()
+        return iv + encryptor.tag + ciphertext
 
-    def encrypt_file(self, file_path):
-        with open(file_path, 'rb') as file:
-            file_data = file.read()
-        return self.encrypt(file_data)
-    
-    def decrypt_file(self, encrypted_file_data, output_file_path):
-        decrypted_data = self.decrypt(encrypted_file_data)
-        with open(output_file_path, 'wb') as file:
-            file.write(decrypted_data)
+    def decrypt_file(self, encrypted_data, shared_key):
+        iv = encrypted_data[:12]
+        tag = encrypted_data[12:28]
+        ciphertext = encrypted_data[28:]
 
-# Example usage
-hasher = BlockCipherHasher()
+        decryptor = Cipher(
+            algorithms.AES(shared_key),
+            modes.GCM(iv, tag),
+            backend=default_backend()
+        ).decryptor()
 
-# Encrypt a text message
-encrypted_text = hasher.encrypt_text("Hello, World!")
-print(f"Encrypted text: {encrypted_text}")
-
-# Decrypt the text message
-decrypted_text = hasher.decrypt_text(encrypted_text)
-print(f"Decrypted text: {decrypted_text}")
-
-# Encrypt a file
-encrypted_file_data = hasher.encrypt_file("path/to/your/file.txt")
-print(f"Encrypted file data: {encrypted_file_data}")
-
-
-# Decrypt the file
-hasher.decrypt_file(encrypted_file_data, "path/to/your/decrypted_file.txt")
-print("File decrypted successfully")
+        return decryptor.update(ciphertext) + decryptor.finalize()
--- KeyManagement.py
+++ KeyManagement.py
@@ -1,13 +1,11 @@
-import os
+
 import sqlite3
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives.hashes import SHA256
 from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-import base64
+from AES import *
 
 class SecureChatManager:
     def __init__(self):
@@ -45,53 +43,20 @@ class SecureChatManager:
         return derived_key
 
     def encrypt_message(self, message, shared_key):
-        iv = os.urandom(12)
-        encryptor = Cipher(
-            algorithms.AES(shared_key),
-            modes.GCM(iv),
-            backend=default_backend()
-        ).encryptor()
-
-        ciphertext = encryptor.update(message.encode()) + encryptor.finalize()
-        return base64.b64encode(iv + encryptor.tag + ciphertext).decode('utf-8')
+        aes = AES()
+        return aes.encrypt_message(message, shared_key)
 
     def decrypt_message(self, encrypted_message, shared_key):
-        encrypted_data = base64.b64decode(encrypted_message.encode('utf-8'))
-        iv = encrypted_data[:12]
-        tag = encrypted_data[12:28]
-        ciphertext = encrypted_data[28:]
-
-        decryptor = Cipher(
-            algorithms.AES(shared_key),
-            modes.GCM(iv, tag),
-            backend=default_backend()
-        ).decryptor()
-
-        return decryptor.update(ciphertext).decode() + decryptor.finalize().decode()
+        aes = AES()
+        return aes.decrypt_message(encrypted_message, shared_key)
 
     def encrypt_file(self, file_data, shared_key):
-        iv = os.urandom(12)
-        encryptor = Cipher(
-            algorithms.AES(shared_key),
-            modes.GCM(iv),
-            backend=default_backend()
-        ).encryptor()
-
-        ciphertext = encryptor.update(file_data) + encryptor.finalize()
-        return iv + encryptor.tag + ciphertext
+        aes = AES()
+        return aes.encrypt_file(file_data, shared_key)
 
     def decrypt_file(self, encrypted_data, shared_key):
-        iv = encrypted_data[:12]
-        tag = encrypted_data[12:28]
-        ciphertext = encrypted_data[28:]
-
-        decryptor = Cipher(
-            algorithms.AES(shared_key),
-            modes.GCM(iv, tag),
-            backend=default_backend()
-        ).decryptor()
-
-        return decryptor.update(ciphertext) + decryptor.finalize()
+        aes = AES()
+        return aes.decrypt_file(encrypted_data, shared_key)
 
 class KeyManagement:
     def __init__(self, db_path="MailSecurity.db"):
